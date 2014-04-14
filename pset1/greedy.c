8473b97f721bf9e13ab44bb8493a83a9084b11dc
Eric Pan <ericpan@gmail.com>
2014-04-14 01:44:13 -0700

    Seems to work

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -9,8 +9,8 @@ int remainder;
 
 int quarters;
 int dimes;
-// int nickels;
-// int pennies;
+int nickels;
+int pennies;
 
 // Gets change owed
 do {
@@ -21,17 +21,25 @@ do {
 
 quarters = change_cents / 25;
 remainder = change_cents % 25;
-printf("Quarters remainder %i.\n", remainder);
+// printf("Quarters remainder %i.\n", remainder);
 
 dimes = remainder / 10;
 remainder = remainder % 10;
-printf("Dimes remainder %i.\n", remainder);
+// printf("Dimes remainder %i.\n", remainder);
 
-printf("You owe %f.\n", change);
-printf("You owe %i cents.\n", change_cents);
+nickels = remainder / 5;
+remainder = remainder % 5;
+// printf("Nickels remainder %i.\n", remainder);
+
+pennies = remainder;
+
+// printf("You owe %f.\n", change);
+// printf("You owe %i cents.\n", change_cents);
 
 printf("%i Quarters\n", quarters);
 printf("%i Dimes\n", dimes);
+printf("%i Nickels\n", nickels);
+printf("%i Pennies\n", pennies);
 
 // printf("Quarters remainder %i.\n", remainder);
 
