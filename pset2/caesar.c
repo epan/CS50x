6a476bfa1f4493439244ec0bcea8c22fd459dc97
Eric Pan <ericpan@gmail.com>
2014-04-30 00:31:16 -0700

    Makes sure only 1 argument is passed

---
+++ pset2/caesar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[]) 
+{
+    if (argc != 2) 
+    {
+        printf("nope\n");
+    } else 
+    {
+        printf("yep\n");
+    }
+    
+}
