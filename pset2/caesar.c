97d093a6d5e6140ba68650422d803f15290d499c
Eric Pan <ericpan@gmail.com>
2014-04-30 10:45:50 -0700

    Comments debug code

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -9,29 +9,35 @@ int main(int argc, string argv[])
     int k;
     string input;
     
-    // Ensure only 1 argument is passed in.
-    if (argc != 2) 
+    // Ensure only 1 argument is passed in
+    if (argc != 2)
     {
         printf("nope\n");
         return 1;
     } else 
     {
-        // Convert string to int.
+        // Convert string to int
         k = atoi(argv[1]);
         printf("k is %i\n", k);
         
         input = GetString();
+        
+        // Debug: Confirm the input string
         printf("input is %s\n", input);
         
         for (int i = 0, n = strlen(input); i < n; i++) 
         {
             int letter = input[i];
+            
+            // Debug: Confirm the letter
             printf("the letter is %c\n", letter);
             
-            // Check to see if character is alpha.
+            // Check to see if character is alpha
             if (isalpha(letter))
             {       
                 int result = (letter + k);
+                
+                // Debug: Confirm the encrypted result
                 printf("the result is %c\n", result);
             } else
             {
