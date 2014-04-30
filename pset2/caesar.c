49a5712249f197fa8e48d30bb1767bb2db9d7b33
Eric Pan <ericpan@gmail.com>
2014-04-30 10:36:14 -0700

    Works with 1 word, no wrapping around alphabet, ignores non alphas

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -2,28 +2,42 @@
 #include <cs50.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, string argv[]) 
 {
     int k;
     string input;
-    // string output;
     
+    // Ensure only 1 argument is passed in.
     if (argc != 2) 
     {
         printf("nope\n");
         return 1;
     } else 
     {
+        // Convert string to int.
         k = atoi(argv[1]);
         printf("k is %i\n", k);
+        
         input = GetString();
         printf("input is %s\n", input);
-       
+        
         for (int i = 0, n = strlen(input); i < n; i++) 
         {
-            printf("%c\n", input[i]);
+            int letter = input[i];
+            printf("the letter is %c\n", letter);
+            
+            // Check to see if character is alpha.
+            if (isalpha(letter))
+            {       
+                int result = (letter + k);
+                printf("the result is %c\n", result);
+            } else
+            {
+                printf("%c\n", input[i]);
+            }
         }
+        return 0;
     }
-    
 }
